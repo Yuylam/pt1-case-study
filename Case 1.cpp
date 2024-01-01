0cc219072f5e8be3c22afbb1b95047213099b79f
Lam Yoke Yu <147635499+Yuylam@users.noreply.github.com>
2024-01-01 20:38:06 +0800

    Update Case 1.cpp

--- Case 1.cpp
+++ Case 1.cpp
@@ -1,5 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
+#include <string>
 using namespace std;
 
 int main(){
-  
+	string name[10], matricNumber[10];
+	char studentAns[10][20], correctAns[20], grade[10];
+	bool correctedAns[20];
+	int percentage[10];
+	int studentNum, studentIndex, missNum;
+	
+	studentNum = readFile(name, matricNumber, studentAns, correctAns);
+	
+	// Get Matric Number
+	
+	// Find student index
+	
+	// Find percentage and grade
+	
+	// Output file
+	
+	missNum = compareAns(studentIndex, studentAns, correctAns, correctedAns);
+	
+	printReport(studentIndex, studentAns, correctAns, correctedAns, percentage, grade);
+	
+	
+	return 0;
+}

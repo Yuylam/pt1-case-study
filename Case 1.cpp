13671eed7dd30e5756b5d4517d33355a9edea03e
Lam Yoke Yu <147635499+Yuylam@users.noreply.github.com>
2024-01-01 21:41:40 +0800

    Update Case 1.cpp

--- Case 1.cpp
+++ Case 1.cpp
@@ -1,33 +1,65 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int readFile(string[][20], string[], char[], char[]);
 int compareAns(int, char[][20], char[], bool[]);
 void printMissQuestion(int, char[][20], char[], bool[]);
-void printReport(int, char[][20], char[], bool[], int[], char[]);
+void printReport(string[], string[], int[], char[]);
 
 int main(){
-	string name[10], matricNumber[10];
-	char studentAns[10][20], correctAns[20], grade[10];
-	bool correctedAns[20];
-	int percentage[10];
-	int studentNum, studentIndex, missNum;
+	string name[10], 				// Student name read from file
+	       matricNumber[10];			// Student ID read from file
+	char studentAns[10][20], 			// Student answer read from file
+	     correctAns[20], 				// Correct answer read from file
+	     grade[10];					// Grade computed of each student
+	bool correctedAns[20];	
+	int percentage[10];				// Percentage computed of each student
+	int studentNum, 
+	    studentIndex = -1, 
+	    missNum;
+	const int QUES_NUM = 20;
+	ofstream outFile;
 	
 	studentNum = readFile(name, matricNumber, studentAns, correctAns);
 	
-	// Get Matric Number
-	
-	// Find student index
-	
 	// Find percentage and grade
+	for (int i = 0; i < QUES_NUM; i++){
+		missNum = compareAns(studentIndex, studentAns, correctAns, correctedAns);
+		percentage[i] = (QUES_NUM - missNum) / static_cast<double>(QUES_NUM) * 100;
+		if(percentage >= 80)
+			grade[i] = 'A';
+		else if(percentage >= 70)
+			grade[i] = 'B';
+		else if(percentage >= 60)
+			grade[i] = 'C';
+		else
+			grade[i]  = 'F';
+	}
+
+	printReport(name, matricNumber, percentage, grade);
 	
-	// Output file
+	// Get Matric Number and find student index
+	do{
+		cout << "Enter the student ID: ";
+		cin >> studentID;
+		for (int i = 0; i < studentNum; i++){
+			if (matricNumber[i] == studentID){
+				studentIndex = i;
+				break;
+			}
+		}
+		if (studentIndex == -1){
+			cout << "The ID does not exist in the database\n";
+		}
+	}
+	while(studentIndex == -1);
+
+	// Print Report
 	
-	missNum = compareAns(studentIndex, studentAns, correctAns, correctedAns);
 	
-	printReport(studentIndex, studentAns, correctAns, correctedAns, percentage, grade);
 	
 	
 	return 0;
